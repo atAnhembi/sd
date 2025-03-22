1c670fa2a5bf9cbf4a3d62ecee31a7f8d5e67e17
Emerson <paduandev@gmail.com>
2025-03-21 21:22:28 -0300

    ex chamada

--- chamada/Request.java
+++ chamada/Request.java
@@ -1,6 +1,7 @@
 import java.io.Serializable;
 
 public class Request implements Serializable {
+    static final long serialVersionUID = 1L;
     private String password;
     private String ra;
 
@@ -13,16 +14,8 @@ public class Request implements Serializable {
         return password;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
     public String getRa() {
         return ra;
-    }
-
-    public void setRa(String ra) {
-        this.ra = ra;
     }
 
 }
--- chamada/Response.java
+++ chamada/Response.java
@@ -1,6 +1,7 @@
 import java.io.Serializable;
 
 public class Response implements Serializable {
+    static final long serialVersionUID = 1L;
     private int cod;
     private String message;
     
