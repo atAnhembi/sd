d4b6567a4c730c96d462836ba795a0769cf14dba
Emerson <paduandev@gmail.com>
2025-03-21 21:09:52 -0300

    ex chamada

---
+++ chamada/Request.java
@@ -0,0 +1,28 @@
+import java.io.Serializable;
+
+public class Request implements Serializable {
+    private String password;
+    private String ra;
+
+    public Request(String password, String ra) {
+        this.password = password;
+        this.ra = ra;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRa() {
+        return ra;
+    }
+
+    public void setRa(String ra) {
+        this.ra = ra;
+    }
+
+}
---
+++ chamada/Response.java
@@ -0,0 +1,29 @@
+import java.io.Serializable;
+
+public class Response implements Serializable {
+    private int cod;
+    private String message;
+    
+    public Response(int cod, String message) {
+        this.cod = cod;
+        this.message = message;
+    }
+
+    public int getCod() {
+        return cod;
+    }
+
+    public void setCod(int cod) {
+        this.cod = cod;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    
+}
